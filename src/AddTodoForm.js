e2e72e9dccc5bbbea009adab86c9ea41b5adcff3
charuzain <charuabhizain@gmail.com>
2023-06-20 11:24:33 -0400

    Add TODoForm submit

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,21 +1,31 @@
 import React from "react";
 
 function AddTodoForm() {
+  const handleAddTodo = (e) => {
+    e.preventDefault()  
+    console.log(e.target.title.value);
+    const todoTitle = e.target.title.value;
+    e.target.reset()
+    
+
+   };
   return (
     <>
       <h1 className="text-center">Add New ToDo Item</h1>
       <div className="m-4 d-flex justify-content-center">
-        <form>
+        <form onSubmit={handleAddTodo}>
           <div className="row mb-3">
             <label htmlFor="todoTitle" className="col-sm-2 col-form-label">
               Title
             </label>
             <div className="col-sm-10">
               <input
+                name = "title"
                 className="form-control"
                 id="todoTitle"
                 placeholder="new todo item"
                 required
+
               />
             </div>
           </div>
